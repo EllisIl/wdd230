ec8a94a333a594fd2b566d4ae243e4ed5186f92a
Elijah Foard <efoard@byui.edu>
2024-01-25 08:11:28 -0700

    added js and fixed warnings

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,1 +1,5 @@
-console.log("Connected!")
+var currentYear = new Date().getFullYear();
+document.getElementById("year").textContent = `${currentYear}${'\u00A9'} | Elijah Foard | USA`;
+
+var lastModified = document.lastModified
+document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
